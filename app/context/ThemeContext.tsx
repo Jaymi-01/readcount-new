6007b5cb9863256ebfa8cc97e6a050bfde16765d
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2026-01-05 01:39:56 +0100

    feat: created the theme

---
+++ app/context/ThemeContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState, useContext, useMemo, ReactNode } from 'react';
+import { Appearance, ColorSchemeName } from 'react-native';
+
+interface ThemeContextType {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: 'light',
+  toggleTheme: () => {},
+});
+
+export const useTheme = () => useContext(ThemeContext);
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>((Appearance.getColorScheme() as 'light' | 'dark') || 'light');
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  const value = useMemo(() => ({ theme, toggleTheme }), [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
