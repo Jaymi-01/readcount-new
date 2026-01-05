dbe4bb8e04f01dcb247a0035335afb5ff27e566b
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2026-01-05 02:35:11 +0100

    imported toast

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
 import { ThemeProvider } from "./context/ThemeContext";
+import Toast from 'react-native-toast-message';
 
 export default function RootLayout() {
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }} />
+      <Toast />
     </ThemeProvider>
   );
 }
